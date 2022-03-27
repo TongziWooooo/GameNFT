ca9a5cf7fbb237142d459cdc91199d5c7b50aa67
yangbowen <bowenyang@buaa.edu.cn>
2022-03-26 21:16:50 -0400

    Before Merge

---
+++ contracts/Marketplace.sol
@@ -0,0 +1,138 @@
+pragma solidity ^0.8.10;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract Marketplace is ReentrancyGuard{
+    using Counters for Counters.Counter;
+    Counters.Counter private _itemID;
+    Counters.Counter private _itemSold;
+
+    // Solidity classify payable or not in 0.5,
+    // address payable could use .transfer() and .send()
+    address payable owner;
+    uint shelfPrice = 0.01 ether;
+
+    constructor(){
+        owner = payable(msg.sender);
+    }
+
+    struct MarketItem {
+        uint itemID;
+        address NFTContract;
+        uint tokenID;
+        address payable seller;
+        address payable owner;
+        uint price;
+        bool isSold;
+    }
+
+    mapping(uint => MarketItem) private idToMarketItem;
+
+    event MarketItemCreated (
+        // at most 3 indexed identifier could be used
+        // in order to be searched or filtered;
+        uint indexed itemID,
+        address indexed nftContract,
+        uint256 indexed tokenID,
+        address seller,
+        address owner,
+        uint256 price,
+        bool sold
+    );
+
+    function getListPrice() public view returns (uint) {
+        return shelfPrice;
+    }
+
+    // nonReentrant Prevents a contract from calling itself, directly or indirectly.
+    function createMarketItem(address nftContract, uint tokenID,
+        uint price) public payable nonReentrant {
+        require(price > 0, "The price should be > 0");
+        require(msg.value == shelfPrice, "Value must equal to shelfPrice");
+
+        _itemID.increment();
+        uint itemID = _itemID.current();
+
+        // Store the market item on the Chain first.
+        idToMarketItem[itemID] =  MarketItem(
+            itemID,
+            nftContract,
+            tokenID,
+            payable(msg.sender),
+            payable(address(0)),
+            price,
+            false
+        );
+
+        // Convert to ERC721 and then transfer from sender to server.
+        IERC721(nftContract).transferFrom(msg.sender, address(this), tokenID);
+
+        emit MarketItemCreated(
+            itemID,
+            nftContract,
+            tokenID,
+            msg.sender,
+            address(0),
+            price,
+            false
+        );
+    }
+
+    //
+    function sellItem(address nftContract, uint itemID) public payable nonReentrant{
+        uint price = idToMarketItem[itemID].price;
+        uint tokenID = idToMarketItem[itemID].tokenID;
+        require(msg.value == price, "Please pay the asking price");
+
+        // Transfer money to seller
+        idToMarketItem[itemID].seller.transfer(msg.value);
+        // Transfer Item to msg.sender
+        IERC721(nftContract).transferFrom(address(this), msg.sender, tokenID);
+        idToMarketItem[itemID].owner = payable(msg.sender);
+        idToMarketItem[itemID].isSold = true;
+        _itemSold.increment();
+    }
+
+    function fetchUnsoldItems() public view returns (MarketItem[] memory) {
+        uint itemCount = _itemID.current();
+        uint unsoldItemCount = _itemID.current() - _itemSold.current();
+        uint curIndex = 0;
+
+        MarketItem[] memory itemList = new MarketItem[](unsoldItemCount);
+        for (uint i = 1; i <= itemCount; i++) {
+            if (idToMarketItem[i].owner == address(0)) {
+                // Stored on the Chain
+                MarketItem storage curItem = idToMarketItem[i];
+                itemList[curIndex] = curItem;
+                curIndex += 1;
+            }
+        }
+        return itemList;
+    }
+
+    function fetchUserNFT() public view returns (MarketItem[] memory) {
+        uint totalItemCount = _itemID.current();
+        uint itemNum = 0;
+        uint curIndex = 0;
+
+        for(uint i = 1; i <= totalItemCount; i++){
+            if(idToMarketItem[i].owner == msg.sender){
+                itemNum += 1;
+            }
+        }
+
+        MarketItem[] memory itemList = new MarketItem[](itemNum);
+
+        for(uint i = 1; i <= totalItemCount; i++){
+            if(idToMarketItem[i].owner == msg.sender){
+                MarketItem storage curItem = idToMarketItem[i];
+                itemList[curIndex] = curItem;
+                curIndex += 1;
+            }
+        }
+        return itemList;
+    }
+
+}
--- src/App.js
+++ src/App.js
@@ -1,24 +1,157 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+//
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+//
+// export default App;
 
+import './App.css';
+import { useState } from 'react';
+import { ethers } from 'ethers'
+import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json'
+import GameNFT from './artifacts/contracts/GameNFT.sol/GameNFT.json'
+import ArenaGame from './artifacts/contracts/ArenaGame.sol/ArenaGame.json'
+
+// Update with the contract address logged out to the CLI when it was deployed
+// const greeterAddress = "your-contract-address"
+const greeterAddress = "0x70e0bA845a1A0F2DA3359C97E0285013525FFC49"
+const gameNFTAddress = "0xcbEAF3BDe82155F56486Fb5a1072cb8baAf547cc"
+const arenaGameAddress = "0x4631BCAbD6dF18D94796344963cB60d44a4136b6"
 function App() {
+  // store greeting in local state
+  const [greeting, setGreetingValue] = useState()
+  const [yourTokenID, setYourTokenID] = useState()
+  const [adversaryTokenID, setAdversaryTokenID] = useState()
+
+  // request access to the user's MetaMask account
+  async function requestAccount() {
+    await window.ethereum.request({ method: 'eth_requestAccounts' });
+  }
+
+  // call the smart contract, read the current greeting value
+  async function fetchGreeting() {
+    if (typeof window.ethereum !== 'undefined') {
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider)
+      try {
+        const data = await contract.greet()
+        console.log('data: ', data)
+      } catch (err) {
+        console.log("Error: ", err)
+      }
+    }
+  }
+
+  // call the smart contract, send an update
+  async function setGreeting() {
+    if (!greeting) return
+    if (typeof window.ethereum !== 'undefined') {
+      await requestAccount()
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner()
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer)
+      const transaction = await contract.setGreeting(greeting)
+      await transaction.wait()
+      fetchGreeting()
+    }
+  }
+
+  //
+  async function mintNFT() {
+    if (typeof window.ethereum !== 'undefined') {
+      await requestAccount()
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner()
+      const contract = new ethers.Contract(gameNFTAddress, GameNFT.abi, signer)
+      const transaction = await contract.mintItem("https://www.google.com", 0)
+      await transaction.wait()
+      console.log(transaction)
+    }
+  }
+
+  async function showAllNFT() {
+    if (typeof window.ethereum !== 'undefined') {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(gameNFTAddress, GameNFT.abi, provider)
+      try {
+        const data = await contract.fetchAllNFT()
+        console.log('data: ', data)
+      } catch (err) {
+        console.log("Error: ", err)
+      }
+    }
+  }
+
+  async function showUserNFT() {
+    await requestAccount()
+    if (typeof window.ethereum !== 'undefined') {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner()
+      const contract = new ethers.Contract(gameNFTAddress, GameNFT.abi, signer)
+      try {
+        const data = await contract.fetchUserNFT()
+        console.log('data: ', data)
+      } catch (err) {
+        console.log("Error: ", err)
+      }
+    }
+  }
+
+  async function burnNFT() {
+
+  }
+
+  async function createGame() {
+    if(!adversaryTokenID) return;
+    console.log(yourTokenID, adversaryTokenID)
+    await requestAccount()
+    if (typeof window.ethereum !== 'undefined') {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner()
+      const contract = new ethers.Contract(arenaGameAddress, ArenaGame.abi, signer)
+      try {
+        const data = await contract.createGame(yourTokenID, adversaryTokenID)
+        console.log('data: ', data)
+      } catch (err) {
+        console.log("Error: ", err)
+      }
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <button onClick={fetchGreeting}>Fetch Greeting</button>
+          <input onChange={e => setGreetingValue(e.target.value)} placeholder="Set greeting"/>
+          <button onClick={setGreeting}>Set Greeting</button>
+
+          <button onClick={mintNFT}>Mint NFT</button>
+          <button onClick={showUserNFT}>Fetch Your NFT</button>
+          <button onClick={showAllNFT}>Fetch All NFT</button>
+          <input onChange={e => setYourTokenID(e.target.value)} placeholder="Your Token ID"/>
+          <input onChange={e => setAdversaryTokenID(e.target.value)} placeholder="Adversary Token ID"/>
+          <button onClick={createGame}>Create Game</button>
+        </header>
+      </div>
   );
 }
 
