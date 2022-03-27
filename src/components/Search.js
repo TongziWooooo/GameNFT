79a35e99cf397320c3ab7dcbe984dc268277266e
Zeng Ruijing <jackjin779@gmail.com>
2022-03-27 14:30:58 -0400

    complete search-by-name in page explore

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,13 +2,13 @@ import React from "react";
 import TextInput from "./base/TextInput";
 import { AiOutlineSearch } from "react-icons/ai";
 
-const Search = () => {
+const Search = ({handleQuery, handleClick, handleKeyDown}) => {
   return (
-    <div id="abc" style={{"position": "relative", "top": "90px"}}>
+    <div style={{"position": "relative", "top": "90px"}}>
       <TextInput
-        icon={<AiOutlineSearch size="30" color="rgba(48,118,234,1)"/>}
+        icon={<AiOutlineSearch onClick={handleClick} size="30" color="rgba(48,118,234,1)"/>}
         child={
-          <input id="search" placeholder={"Explore NFTs"} />
+          <input id="search" placeholder={"Explore NFTs"} onChange={handleQuery} onKeyDown={handleKeyDown}/>
         }
       />
     </div>
--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,5 +1,5 @@
 import react from "react";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import CardList from "../components/CardList";
 import {exploreList, hotDropsData} from "../constants/MockupData";
 import '../styles/Explore.css';
@@ -9,27 +9,47 @@ import Moralis from "moralis";
 
 
 const Explore = () => {
-  const NFT = Moralis.Object.extend("NFT");
-  const agent = new Moralis.Query(NFT);
 
+  const parsedResults = useRef([]);
   const [exploreData, setExploreData] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
-
+      const NFT = Moralis.Object.extend("NFT");
+      const agent = new Moralis.Query(NFT);
       const results = await agent.find();
-      const parsed_results = results.map((item) => (
+      parsedResults.current = results.map((item) => (
         item.attributes
       ))
-      console.log(parsed_results)
-      setExploreData(parsed_results);
+      console.log(parsedResults.current)
+      setExploreData(parsedResults.current);
     }
     fetchData();
   }, [])
 
+  const [query, setQuery] = useState("");
+  useEffect(() => {
+    console.log(query)
+  }, [query])
+  const handleQuery = (e) => {
+    setQuery(e.target.value);
+  }
+
+  const handleClick = () => {
+    setExploreData(parsedResults.current.filter(item => (
+      item.name.toLowerCase().includes(query.toLowerCase())
+    )))
+  }
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter")
+      handleClick();
+  }
+
+
   return (
     <div id="explore">
       <Header />
-      <Search/>
+      <Search handleQuery={handleQuery} handleClick={handleClick} handleKeyDown={handleKeyDown}/>
       <div id="list-container" style={{"margin-top": "90px"}}>
         <CardList list={exploreData} />
       </div>
